d351f4b205d7d841b866e738040a26d105fb9bad
Ian Chen <ianchen@eztravel.com.tw>
2015-03-16 17:40:56 +0800

    config調整成從命令列參數傳入

--- Java/mybatis-generator-core-1.3.2/gen.sh
+++ Java/mybatis-generator-core-1.3.2/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # 需先在執行目錄下建立資料夾：output\src
-java -jar lib/mybatis-generator-core-1.3.2.jar -configfile config/generatorConfig.xml -overwrite
+java -jar lib/mybatis-generator-core-1.3.2.jar -configfile $1 -overwrite

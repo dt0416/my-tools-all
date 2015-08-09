d1861aeaae830f239b9aef41e77a34f3577ce35d
Ian <dt0416@gmail.com>
2015-08-09 20:12:03 +0800

    增加gradle的設定

--- Java/SetEnv.bat
+++ Java/SetEnv.bat
@@ -2,11 +2,12 @@
 rem ==========================================================
 rem java, ant, maven
 rem ==========================================================
-rem set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_02
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_55
 set ANT_HOME=D:\Develop_Env\Java\apache-ant-1.8.3
 set M2_HOME=D:\Develop_Env\Java\apache-maven-3.0.4
 REM set M2_HOME=D:\java\eclipse\eclipse-4.2.1-DMP\apache-maven-2.2.1
 set CXF_HOME=D:\Develop_Env\Java\apache-cxf-2.1.4
+set GRADLE_HOME=D:\Develop_Env\Java\gradle-2.5
 
 rem ==========================================================
 rem axis2
@@ -21,6 +22,6 @@ set COffeeScript_HOME=D:\Develop_Env\Javascript\CoffeeScript-Compiler-for-Window
 rem ==========================================================
 rem PATH
 rem ==========================================================
-set PATH=.;%ANT_HOME%\bin;%M2_HOME%\bin;%AXIS2_HOME%\bin;%COffeeScript_HOME%;%PATH%
+set PATH=.;%ANT_HOME%\bin;%M2_HOME%\bin;%AXIS2_HOME%\bin;%COffeeScript_HOME%;%GRADLE_HOME%\bin;%PATH%
 
 rem @echo on
